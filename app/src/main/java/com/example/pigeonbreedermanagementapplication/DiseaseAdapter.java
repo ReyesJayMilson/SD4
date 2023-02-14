41b9ffaf779cde8585bb42e13a238b280dafbdc9
DetTapteSjel <evanlancelot66@gmail.com>
2023-02-14 15:04:17 +0800

    Disease Library Clickable na pero walang function hehe

--- app/src/main/java/com/example/pigeonbreedermanagementapplication/DiseaseAdapter.java
+++ app/src/main/java/com/example/pigeonbreedermanagementapplication/DiseaseAdapter.java
@@ -1,5 +1,8 @@
 package com.example.pigeonbreedermanagementapplication;
 
+import static com.google.android.material.internal.ContextUtils.getActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +17,8 @@ import java.util.ArrayList;
 
 public class DiseaseAdapter extends RecyclerView.Adapter<DiseaseAdapter.DiseaseViewHolder> {
     private ArrayList<Disease> diseaseList = new ArrayList<>();
+    private int selectedPos = RecyclerView.NO_POSITION;
+    private Context context;
 
     private void add(Disease disease){
         diseaseList.add(disease);
@@ -34,6 +39,16 @@ public class DiseaseAdapter extends RecyclerView.Adapter<DiseaseAdapter.DiseaseV
         Disease disease = diseaseList.get(position);
         holder.textName.setText(disease.getName());
         holder.textDesc.setText(disease.getDesc());
+
+        holder.itemView.setSelected(selectedPos == position);
+//        holder.itemView.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View view){
+//          //      Intent intent = new Intent(context)
+//                System.out.print("Clicked");
+//            }
+//        });
     }
 
     @Override
@@ -49,13 +64,29 @@ public class DiseaseAdapter extends RecyclerView.Adapter<DiseaseAdapter.DiseaseV
 
 
 
-    static class DiseaseViewHolder extends RecyclerView.ViewHolder {
+    public class DiseaseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
         TextView textName, textDesc;
 
         DiseaseViewHolder(@NonNull View itemView){
             super(itemView);
             textName = itemView.findViewById(R.id.textName);
             textDesc = itemView.findViewById(R.id.textDesc);
+
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+        }
+
+        public void onClick(View view){
+            Toast.makeText(view.getContext(), textName.getText() + " clicked", Toast.LENGTH_SHORT).show();
+            selectedPos = getAdapterPosition();
+            notifyItemChanged(selectedPos);
+        }
+
+        public boolean onLongClick(View v){
+            int oldPos = selectedPos;
+            selectedPos = getAdapterPosition();
+            notifyItemChanged(selectedPos);
+            return true;
         }
     }
 }
